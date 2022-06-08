0d0253511e08c093677e143e1efcced4a75ecc77
bruceeewong <292803281@qq.com>
2022-06-08 17:36:23 +0800

    chore: rewrite implementation

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -5,8 +5,8 @@
 
 template<typename T>
 void bubbleSort(T arr[], int n) {
+    // do-while loop implementation
     bool isSwapped;
-
     do {
         isSwapped = false;
         for (int i = 1; i < n; i++) {
@@ -17,37 +17,49 @@ void bubbleSort(T arr[], int n) {
         }
         n--;  // 每一次都将最大元素放到最后, 下一次循环不再考虑
     } while (isSwapped);
+
+    // for loop implementation
+//    for (int i = n - 1; i > 0; i--) {
+//        bool isSwapped = false;
+//        for (int j = 0; j < i; j++) {
+//            if (arr[j] > arr[j + 1]) {
+//                swap(arr[j], arr[j + 1]);
+//                isSwapped = true;
+//            }
+//        }
+//        if (!isSwapped) break;
+//    }
 }
 
 template<typename T>
 void bubbleSort2(T arr[], int n) {
-    int newn;
-
+    int lastN;
     do {
-        newn = 0;
+        lastN = 0;
         for (int i = 1; i < n; i++) {
             if (arr[i - 1] > arr[i]) {
                 swap(arr[i - 1], arr[i]);
-                newn = i;  // 记录最后一次交换的位置, 下一轮直接以此位置为n
+                // 记录最后一次的交换位置,在此之后的元素在下一轮扫描中均不考虑
+                lastN = i;
             }
         }
-        n = newn;
-    } while (newn > 0);
+        n = lastN;  // 如果后面已经排好序了，下一轮可以直接只计算未排序的部分
+    } while (lastN > 0);
 }
 
 int main() {
     int n = 20000;
 
     // 测试1 一般测试
-    cout<<"Test for random array, size = "<<n<<", randome range [0, "<<n<<"]"<<endl;
+    cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
 
-    int *arr1 = SortTestHelper::generateRandomArray(n,0,n);
+    int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
     int *arr2 = SortTestHelper::copyIntArray(arr1, n);
     int *arr3 = SortTestHelper::copyIntArray(arr1, n);
     int *arr4 = SortTestHelper::copyIntArray(arr1, n);
 
-    SortTestHelper::testSort("Selection Sort", selectionSort, arr1, n);
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
+//    SortTestHelper::testSort("Selection Sort", selectionSort, arr1, n);
+//    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
     SortTestHelper::testSort("Bubble Sort", bubbleSort, arr3, n);
     SortTestHelper::testSort("Bubble Sort 2", bubbleSort2, arr4, n);
 
@@ -59,15 +71,15 @@ int main() {
     // 测试2 测试近乎有序的数组
     int swapTimes = 100;
 
-    cout<<"Test for nNearly ordered array, size = "<<n<<", swap time = "<<swapTimes<<endl;
+    cout << "Test for Nearly ordered array, size = " << n << ", swap time = " << swapTimes << endl;
 
     arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
     arr3 = SortTestHelper::copyIntArray(arr1, n);
     arr4 = SortTestHelper::copyIntArray(arr1, n);
 
-    SortTestHelper::testSort("Selection Sort", selectionSort, arr1, n);
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
+//    SortTestHelper::testSort("Selection Sort", selectionSort, arr1, n);
+//    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
     SortTestHelper::testSort("Bubble Sort", bubbleSort, arr3, n);
     SortTestHelper::testSort("Bubble Sort 2", bubbleSort2, arr4, n);
 
@@ -76,14 +88,14 @@ int main() {
     delete[] arr3;
     delete[] arr4;
 
-    cout<<endl;
+    cout << endl;
 
     // 测试3 测试完全有序的数组
     // 对于完全有序的数组，冒泡排序法也将成为O(n)级别的算法
     swapTimes = 0;
     n = 10000000;    // 由于插入排序法和冒泡排序法在完全有序的情况下都将成为O(n)算法
     // 所以我们的测试数据规模变大，为1000,0000
-    cout<<"Test for ordered array, size = " << n << endl;
+    cout << "Test for ordered array, size = " << n << endl;
 
     arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
@@ -92,7 +104,7 @@ int main() {
 
     // 在这种情况下，不再测试选择排序算法
     //SortTestHelper::testSort("Selection Sort", selectionSort, arr1, n);
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
+//    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
     SortTestHelper::testSort("Bubble Sort", bubbleSort, arr3, n);
     SortTestHelper::testSort("Bubble Sort 2", bubbleSort2, arr4, n);
 
